e015b6ab711b7a5965baf45bf369377562a1bfc5
alexpeloquin <46466270+alexpeloquin@users.noreply.github.com>
2019-02-20 12:08:33 -0800

    Add files via upload
    Unable to get playlist_store.js file to function with remainder of code. Receive errors within console log that "localStorage" is not defined throughout playlist_store.js

---
+++ playlist_store.js
@@ -0,0 +1,32 @@
+function save(Item) {
+    var playlistArray = getStoreArray("playlist");
+    localStorage.setItem("playlist", JSON.stringify(playlistArray));
+
+}
+
+function loadPlaylist() {
+    var playlistArray = getSavedSongs();
+    var ul = document.getElementById("playlist");
+    if (playlistArray !=null) {
+        for (var i=0; i<playlistArray.length; i++) {
+            var li = document.createElement("li");
+            li.innerHTML = playlistArray(i);
+            ul.appendChild(li);
+        }
+    }
+}
+
+function getSavedSongs () {
+    return getStoreArray("playlist");
+}
+
+function getStoreArray(key) {
+    var playlistArray = localStore.getItem(key);
+    if (playlistArray == null || playlistArray =="") {
+        playlistArray = new Array();
+    }
+    else {
+        playlistArray = JSON.parse(playlistArray);
+    }
+    return playlistArray;
+}
